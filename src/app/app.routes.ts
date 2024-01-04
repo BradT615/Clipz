11e682a518f5078b439dfbbfea3636fa65c4a9c4
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-01-03 23:00:54 -0500

    add home and second route components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AnotherPageComponent } from './another-page/another-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'another-page', component: AnotherPageComponent }
+];
