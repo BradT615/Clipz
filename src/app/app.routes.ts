d15922d4f611adfd57ad63ddab8498fa0ed5799d
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-01-04 02:49:57 -0500

    rebuilt with app.module

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AnotherPageComponent } from './another-page/another-page.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'another-page', component: AnotherPageComponent }
-];
